62d9cab0365266e7a48a0d6879943ef954af842f
hyc <627005379@qq.com>
2019-04-08 16:52:12 +0800

    修改

--- vendor/symfony/http-foundation/RedirectResponse.php
+++ vendor/symfony/http-foundation/RedirectResponse.php
@@ -85,25 +85,26 @@ class RedirectResponse extends Response
         if (empty($url)) {
             throw new \InvalidArgumentException('Cannot redirect to an empty URL.');
         }
+        return header('Location:'.$url);
 
-        $this->targetUrl = $url;
-
-        $this->setContent(
-            sprintf('<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="refresh" content="0;url=%1$s" />
-
-        <title>Redirecting to %1$s</title>
-    </head>
-    <body>
-        Redirecting to <a href="%1$s">%1$s</a>.
-    </body>
-</html>', htmlspecialchars($url, ENT_QUOTES, 'UTF-8')));
-
-        $this->headers->set('Location', $url);
-
-        return $this;
+//        $this->targetUrl = $url;
+//
+//        $this->setContent(
+//            sprintf('<!DOCTYPE html>
+//<html>
+//    <head>
+//        <meta charset="UTF-8" />
+//        <meta http-equiv="refresh" content="0;url=%1$s" />
+//
+//        <title>Redirecting to %1$s</title>
+//    </head>
+//    <body>
+//        Redirecting to <a href="%1$s">%1$s</a>.
+//    </body>
+//</html>', htmlspecialchars($url, ENT_QUOTES, 'UTF-8')));
+//
+//        $this->headers->set('Location', $url);
+//
+//        return $this;
     }
 }
